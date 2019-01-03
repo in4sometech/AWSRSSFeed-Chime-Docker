31bcb6d871936ebd0ccb17d15405037448b20bfe
in4sometech <in4sometech@gmail.com>
2019-01-03 14:17:35 -0800

    Create RSSscript.sh

---
+++ RSSscript.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+RSS_URL=https://aws.amazon.com/new/feed/
+newsfeed=$(wget ${RSS_URL} -O - 2>/dev/null | xmlstarlet sel -t -m "/rss/channel/item" -n -v "title" -n -n)
+
+echo "${newsfeed}" | while read line
+do
+   curl -X POST "https://hooks.chime.aws/incomingwebhooks/tokencode" -H "Content-Type:application/json" --data "{\"Content\": \"$line\"}"
+done
