9377b2a84deef169eb54f1e195f3a8aad3745e76
in4sometech <in4sometech@gmail.com>
2019-10-11 19:50:52 -0700

    Update RSSscript.sh

--- RSSscript.sh
+++ RSSscript.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-RSS_URL=https://aws.amazon.com/new/feed/
+#RSS_URL=https://aws.amazon.com/new/feed/
+RSS_URL=https://aws.amazon.com/about-aws/whats-new/recent/feed/
 newsfeed=$(wget ${RSS_URL} -O - 2>/dev/null | xmlstarlet sel -t -m "/rss/channel/item" -n -v "title" -n -n)
 
 echo "${newsfeed}" | while read line
